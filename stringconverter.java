3279cb75797e86ffdf3407ea63e06d11832ad779
jacobtfn <jacob.tfn@gmail.com>
2023-06-20 22:27:16 +0100

    Completion

--- stringconverter.java
+++ stringconverter.java
@@ -3,54 +3,37 @@ import java.util.Arrays;
 
 class StringConverter {
 
-    public static Integer patternValue(ArrayList<Integer> keypattern) {
-        if (keypattern.isEmpty()){
-            return 0;
-        }
-        return 1;
-    }
-
-    public static Integer patternValue(Integer i) {
+    public static Integer stringconverter(String stringkey) {
 
         ArrayList<Integer> keypattern = new ArrayList<>(Arrays.asList(7, 3, 1));
-        return keypattern.get[(i % keypattern.size())];
-
-    }
-
-    public static void stringconverter(String stringkey) {
-
-        ArrayList<Integer> stringkeyconversion = new ArrayList<Integer>();
-        ArrayList<Integer> keypattern = new ArrayList<>(Arrays.asList(7, 3, 1));
-        Integer sumOfValues = 0;
-        
+        Integer summationOfValues = 0;
         
         for (int i = 0; i < stringkey.length(); i++) {
 
-            System.out.println(keypattern.toString());
-            System.out.println("Al valor " + i + " le sumo " + keypattern.get((i % keypattern.size())));
+            if (!keypattern.isEmpty()){
+                summationOfValues = summationOfValues + keypattern.get((i % keypattern.size()));
+            }
 
             if (stringkey.charAt(i) == '>') {
-                stringkeyconversion.add(0);
+                continue;
 
             } else if (Character.getNumericValue(stringkey.charAt(i)) == -1) {
                 System.out.println("ERROR: INVALID KEY.");
-                return;
+                return -1;
 
             } else {
-                stringkeyconversion.add(Character.getNumericValue(stringkey.charAt(i)));
+                summationOfValues = summationOfValues + Character.getNumericValue(stringkey.charAt(i));
             }
 
-
-            System.out.println("Va quedando así: " + stringkeyconversion.toString());
-
         }
+
+        return (summationOfValues % 10);
 
     } 
 
     public static void main(String[] args) {  
-            System.out.println("Hello World");
-            StringConverter.stringconverter("A1B02>23Z+0A5Ñ");
-            StringConverter.stringconverter("A1B02>23ZT0A5N");  
+            System.out.println(StringConverter.stringconverter("A1B02>23Z+0A5Ñ"));
+            System.out.println(StringConverter.stringconverter("A1B02>23ZT0A5N"));  
     }  
 
 }
